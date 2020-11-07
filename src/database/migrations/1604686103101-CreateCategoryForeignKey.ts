c0d6d34ade8422024cba46cf3ebc53433165fd14
fernandomarca <fernandomarca@hotmail.com>
2020-11-07 04:42:58 -0300

    upload

---
+++ src/database/migrations/1604686103101-CreateCategoryForeignKey.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateCategoryForeignKey1604686103101
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'Category',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'Category');
+  }
+}
